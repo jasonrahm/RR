4fa4df2d93c91a098853df433ba696ebb206222b
Jason Rahm <j.rahm@f5.com>
2019-01-14 17:50:05 -0600

    major changes - updated scouting form - run report form - report result form - dashboard prepped for adding scoring - table sorting included - alert for delete record attempt - changed reliability factors from int to float

--- app/forms.py
+++ app/forms.py
@@ -22,53 +22,52 @@ class ScoutingForm(Form):
     team_number = IntegerField('Team Number')
     team_name = StringField('Team Name', [validators.Length(min=1, max=50)])
     comp = SelectField('Competition', choices=[
-        ('IL Southern Meet #1', 'IL Southern Meet #1'),
-        ('IL Southern Meet #2', 'IL Southern Meet #2'),
-        ('IL Southern Meet #3', 'IL Southern Meet #3'),
-        ('IL South/Central East Qualifier', 'IL South/Central East Qualifier'),
-        ('IL State', 'IL State'),
-        ('Detroit World Championships', 'Detroit World Championships'),
+        ('Meet3', 'Meet3'),
+        ('Qualifier', 'Qualifier'),
+        ('State', 'State'),
+        ('Worlds', 'Worlds'),
     ])
     a_lander_loc = SelectField('Lander Location Preference?', choices=[
         ('Crater Side', 'Crater Side'),
         ('Depot Side', 'Depot Side'),
     ])
     a_landed = BooleanField("Can They Land?", default=False)
-    a_landed_rel = SelectField("Landing Reliability?", choices=[
+    a_landed_rel = SelectField("Landing Reliability", choices=[
         (0, '<50%'),
-        (1, '50'),
-        (2, '75'),
-        (3, '85'),
-        (5, '95'),
-    ], coerce=int)
-    a_sample = BooleanField("Can They Sample", default=False)
-    a_sample_rel = SelectField("Sampling Reliability?", choices=[
+        (.5, '50'),
+        (.75, '75'),
+        (.85, '85'),
+        (.95, '95'),
+    ], coerce=float)
+    a_sample = BooleanField("Can They Sample?", default=False)
+    a_sample_rel = SelectField("Sampling Reliability", choices=[
         (0, '<50%'),
-        (1, '50'),
-        (2, '75'),
-        (3, '85'),
-        (5, '95'),
-    ], coerce=int)
+        (.5, '50'),
+        (.75, '75'),
+        (.85, '85'),
+        (.95, '95'),
+    ], coerce=float)
     a_marker = BooleanField("Can They Deliver the Marker?", default=False)
-    a_marker_rel = SelectField("Marker Reliability?", choices=[
+    a_marker_rel = SelectField("Marker Reliability", choices=[
         (0, '<50%'),
-        (1, '50'),
-        (2, '75'),
-        (3, '85'),
-        (5, '95'),
-    ], coerce=int)
+        (.5, '50'),
+        (.75, '75'),
+        (.85, '85'),
+        (.95, '95'),
+    ], coerce=float)
     a_park = BooleanField("Can They Park?", default=False)
-    a_park_rel = SelectField("Parking Reliability?", choices=[
+    a_park_rel = SelectField("Parking Reliability", choices=[
         (0, '<50%'),
-        (1, '50'),
-        (2, '75'),
-        (3, '85'),
-        (5, '95'),
-    ], coerce=int)
+        (.5, '50'),
+        (.75, '75'),
+        (.85, '85'),
+        (.95, '95'),
+    ], coerce=float)
     a_compatible = SelectField("How Compatible Are They?", choices=[
-        (0, 'low'),
-        (1, 'medium'),
-        (3, 'high'),
+        (0, 'not'),
+        (1, 'low'),
+        (3, 'medium'),
+        (5, 'high'),
     ], coerce=int)
     a_notes = StringField("Autonomous Notes", [validators.Length(max=500)])
     t_score_lander = BooleanField("Can They Score in the Lander?", default=False)
@@ -78,11 +77,17 @@ class ScoutingForm(Form):
         ('Silver', 'Silver'),
         ('Both', 'Both'),
     ])
-    t_cycle = IntegerField("What is Their Cycle Time?")
+    t_cycle = SelectField("What is Their Cycle Time?", choices=[
+        (10, '0-5 sec'),
+        (7, '5-10 sec'),
+        (3, '10-15 sec'),
+        (2, '15-20 sec'),
+        (1, '20+ sec'),
+    ], coerce=int)
     t_load = SelectField("What is Their Load Size?", choices=[
         (0, '0'),
         (1, '1'),
-        (3, '2'),
+        (5, '2'),
     ], coerce=int)
     t_score_position = SelectField("Where is Their Preferred Scoring Position?", choices=[
         ('Gold Center', 'Gold Center'),
@@ -94,12 +99,80 @@ class ScoutingForm(Form):
     ])
     t_notes = StringField("TeleOp Notes", [validators.Length(max=500)])
     e_hang = BooleanField("Can They Hang?", default=False)
-    e_hang_rel = SelectField("Hanging Reliability?", choices=[
+    e_hang_rel = SelectField("Hanging Reliability", choices=[
         (0, '<50%'),
-        (1, '50'),
-        (2, '75'),
-        (3, '85'),
-        (5, '95'),
-    ], coerce=int)
-    e_hangtime = IntegerField("How Many Seconds Remaining When They Hang?")
+        (.5, '50'),
+        (.75, '75'),
+        (.85, '85'),
+        (.95, '95'),
+    ], coerce=float)
+    e_hangtime = IntegerField("How Many Seconds Remaining When They Hang?", default=0)
     e_notes = StringField("Endgame Notes", [validators.Length(max=500)])
+
+
+class ScoutingReportForm(Form):
+    comp = SelectField('Competition', choices=[
+        ('Meet3', 'Meet3'),
+        ('Qualifier', 'Qualifier'),
+        ('State', 'State'),
+        ('Worlds', 'Worlds'),
+    ])
+    landing = SelectField('Landing',
+                       choices=[(0, 'Ignore'),
+                                (1, 'Important'),
+                                (4, 'Critical')],
+                       coerce=int,
+                       default=1)
+    landing_rel = BooleanField('Landing Reliability', default=True)
+    sampling = SelectField('Sampling',
+                                  choices=[(0, 'Ignore'),
+                                           (1, 'Important'),
+                                           (4, 'Critical')],
+                                  coerce=int,
+                                  default=1)
+    sampling_rel = BooleanField('Sampling Reliability', default=True)
+    marker = SelectField('Marker Delivery',
+                           choices=[(0, 'Ignore'),
+                                    (1, 'Important'),
+                                    (4, 'Critical')],
+                           coerce=int,
+                           default=1)
+    marker_rel = BooleanField('Marker Reliability', default=True)
+    parking = SelectField('Parking',
+                         choices=[(0, 'Ignore'),
+                                  (1, 'Important'),
+                                  (4, 'Critical')],
+                         coerce=int,
+                         default=0)
+    parking_rel = BooleanField('Parking Reliability', default=False)
+    compatibility = SelectField('Compatibility',
+                         choices=[(0, 'Ignore'),
+                                  (1, 'Important'),
+                                  (4, 'Critical')],
+                         coerce=int,
+                         default=1)
+    lander_scoring = SelectField('Lander Scoring',
+                                choices=[(0, 'Ignore'),
+                                         (1, 'Important'),
+                                         (4, 'Critical')],
+                                coerce=int,
+                                default=1)
+    cycle_time = SelectField('Cycle Time',
+                                 choices=[(0, 'Ignore'),
+                                          (1, 'Important'),
+                                          (4, 'Critical')],
+                                 coerce=int,
+                                 default=1)
+    load_size = SelectField('Load Size',
+                          choices=[(0, 'Ignore'),
+                                   (1, 'Important'),
+                                   (4, 'Critical')],
+                          coerce=int,
+                          default=0)
+    hanging = SelectField('Hanging',
+                          choices=[(0, 'Ignore'),
+                                   (1, 'Important'),
+                                   (4, 'Critical')],
+                          coerce=int,
+                          default=4)
+    hanging_rel = BooleanField('Hanging Reliability', default=True)
--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,100 +1,109 @@
-var a1 = 0;
-var a1m = 0;
-var a2 = 0;
-var a2m = 0;
-var a3 = 0;
-var a4 = 0;
-var t1 = 0;
-var t1m = 0;
-var t2 = 0;
-var t3 = 0;
-var t4 = 0;
-var t5 = 0;
-var t5m = 0;
-var t6 = 0;
+// var a1 = 0;
+// var a1m = 0;
+// var a2 = 0;
+// var a2m = 0;
+// var a3 = 0;
+// var a4 = 0;
+// var t1 = 0;
+// var t1m = 0;
+// var t2 = 0;
+// var t3 = 0;
+// var t4 = 0;
+// var t5 = 0;
+// var t5m = 0;
+// var t6 = 0;
 
 jQuery(document).ready(function() {
-    $("#a_jewel").change( function() {
-       if ( $(this).is(":checked") ) {
-           a1 = 30;
-       } else { a1 = 0; }
+    // $("#a_jewel").change( function() {
+    //    if ( $(this).is(":checked") ) {
+    //        a1 = 30;
+    //    } else { a1 = 0; }
+    // });
+    // $("#a_glyphs").change( function() {
+    //     a2 = parseInt($(this).val()) * 15;
+    // });
+    // $("#a_hit_jewel").change( function() {
+    //    if ( $(this).is(":checked") ) {
+    //        a1m = 30;
+    //    } else { a1m = 0; }
+    // });
+    // $("#a_glyphs_delivered").change( function() {
+    //     a2m = parseInt($(this).val()) * 15;
+    // });
+    // $("#a_glyph_correct").change( function() {
+    //    if ( $(this).is(":checked") ) {
+    //        a3 = 30;
+    //    } else { a3 = 0; }
+    // });
+    // $("#a_park").change( function() {
+    //    if ( $(this).is(":checked") ) {
+    //        a4 = 10;
+    //    } else { a4 = 0; }
+    // });
+    // $("#t_glyphs").change( function() {
+    //     t1 = parseInt($(this).val()) * 2;
+    // });
+    // $("#t_glyphs_delivered").change( function() {
+    //     t1m = parseInt($(this).val()) * 2;
+    // });
+    // $("#t_crypto_columns").change( function() {
+    //     t2 = parseInt($(this).val()) * 20;
+    // });
+    // $("#t_crypto_rows").change( function() {
+    //     t3 = parseInt($(this).val()) * 10;
+    // });
+    // $("#t_crypto_cipher").change( function() {
+    //    if ( $(this).is(":checked") ) {
+    //        t4 = 30;
+    //    } else { t4 = 0; }
+    // });
+    // $("select[name=t_relic1]").change(function() {
+    //     t5 = parseInt($(this).val());
+    //     $('#score_projection').trigger('change');
+    //     $('#t_score').trigger('change');
+    //     $('#total_score').trigger('change');
+    // });
+    // $("select[name=t_relic2]").change(function() {
+    //     t5m = parseInt($(this).val());
+    //     $('#score_projection').trigger('change');
+    //     $('#t_score').trigger('change');
+    //     $('#total_score').trigger('change');
+    // });
+    // $("#t_park").change( function() {
+    //    if ( $(this).is(":checked") ) {
+    //        t6 = 20;
+    //    } else { t6 = 0; }
+    // });
+    // $("input").change(function(){
+    //     // console.log(a1, a2, a3, a4, t1, t2, t3, t4, t5, t6 );
+    //     $("#score_projection").val(a1 + a2 + a3 + a4 + t1 + t2 + t3 + t4 + t5 + t5m + t6);
+    // });
+    // $("input").change(function(){
+    //    $("#a_score").val(a1m + a2m + a3 + a4);
+    // });
+    // $("input").change(function(){
+    //    $("#t_score").val(t1m + t2 + t3 + t4 + t5 + t5m + t6);
+    // });
+    // $("input").change(function(){
+    //    $("#total_score").val(a1m + a2m + a3 + a4 + t1m + t2 + t3 + t4 + t5 + t5m + t6);
+    // });
+    //
+    // $("#matchreport").tablesorter({
+    //         sortList: [[1,1]] // etc.
+    //
+    // });
+    // $("#pitreport").tablesorter({
+    //     sortList: [[1,1]]
+    // });
+    //
+    // $('table.sticky-header').floatThead();
+
+    // Sorting Tables
+    $("#scoutingrecords").tablesorter({
+        sortList: [[1,0]]
     });
-    $("#a_glyphs").change( function() {
-        a2 = parseInt($(this).val()) * 15;
-    });
-    $("#a_hit_jewel").change( function() {
-       if ( $(this).is(":checked") ) {
-           a1m = 30;
-       } else { a1m = 0; }
-    });
-    $("#a_glyphs_delivered").change( function() {
-        a2m = parseInt($(this).val()) * 15;
-    });
-    $("#a_glyph_correct").change( function() {
-       if ( $(this).is(":checked") ) {
-           a3 = 30;
-       } else { a3 = 0; }
-    });
-    $("#a_park").change( function() {
-       if ( $(this).is(":checked") ) {
-           a4 = 10;
-       } else { a4 = 0; }
-    });
-    $("#t_glyphs").change( function() {
-        t1 = parseInt($(this).val()) * 2;
-    });
-    $("#t_glyphs_delivered").change( function() {
-        t1m = parseInt($(this).val()) * 2;
-    });
-    $("#t_crypto_columns").change( function() {
-        t2 = parseInt($(this).val()) * 20;
-    });
-    $("#t_crypto_rows").change( function() {
-        t3 = parseInt($(this).val()) * 10;
-    });
-    $("#t_crypto_cipher").change( function() {
-       if ( $(this).is(":checked") ) {
-           t4 = 30;
-       } else { t4 = 0; }
-    });
-    $("select[name=t_relic1]").change(function() {
-        t5 = parseInt($(this).val());
-        $('#score_projection').trigger('change');
-        $('#t_score').trigger('change');
-        $('#total_score').trigger('change');
-    });
-    $("select[name=t_relic2]").change(function() {
-        t5m = parseInt($(this).val());
-        $('#score_projection').trigger('change');
-        $('#t_score').trigger('change');
-        $('#total_score').trigger('change');
-    });
-    $("#t_park").change( function() {
-       if ( $(this).is(":checked") ) {
-           t6 = 20;
-       } else { t6 = 0; }
-    });
-    $("input").change(function(){
-        // console.log(a1, a2, a3, a4, t1, t2, t3, t4, t5, t6 );
-        $("#score_projection").val(a1 + a2 + a3 + a4 + t1 + t2 + t3 + t4 + t5 + t5m + t6);
-    });
-    $("input").change(function(){
-       $("#a_score").val(a1m + a2m + a3 + a4);
-    });
-    $("input").change(function(){
-       $("#t_score").val(t1m + t2 + t3 + t4 + t5 + t5m + t6);
-    });
-    $("input").change(function(){
-       $("#total_score").val(a1m + a2m + a3 + a4 + t1m + t2 + t3 + t4 + t5 + t5m + t6);
+    $("#scoutingreport").tablesorter({
+        sortList: [[4,0]]
     });
 
-    $("#matchreport").tablesorter({
-            sortList: [[1,1]] // etc.
-
-    });
-    $("#pitreport").tablesorter({
-        sortList: [[1,1]]
-    });
-
-    $('table.sticky-header').floatThead();
 });
